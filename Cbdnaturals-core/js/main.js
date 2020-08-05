fb73caf7cdbcb56441ed573d0908f8ade1256066
asaghir <asaghir@3dcart.com>
2020-08-05 16:02:39 -0400

    Preventing unnecessary ajax calls

--- Cbdnaturals-core/js/main.js
+++ Cbdnaturals-core/js/main.js
@@ -1,4 +1,11 @@
 function update_flyoutcart() {
+	if (window["_3d_cart"] != undefined) {
+        if (_3d_cart.oid == 0)
+            return;
+        jQuery('#floating-cart .minicart-items').text(_3d_cart.itemsum);
+        jQuery('#floating-cart').fadeIn(300);
+        return;
+    }
     jQuery.ajax({
         url: '/frontapi.asp',
         dataType: 'json',
